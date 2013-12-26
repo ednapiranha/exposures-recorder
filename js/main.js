e8020ea89ea2e7c48c1520bc03acdce0683f4a76
Jennifer Fong <jen@ednapiranha.com>
2013-12-25 19:43:54 -0800

    initial recording functionality

---
+++ js/main.js
@@ -0,0 +1,101 @@
+define(['jquery', 'asyncStorage', 'recorder', 'streamer'],
+  function ($, asyncStorage, Recorder, Streamer) {
+  'use strict';
+
+  // This is where we change the time lapse count
+  var interval = 5.0; // In seconds
+  var canvas = document.createElement('canvas');
+  var streamer = new Streamer();
+  var recorder = new Recorder();
+
+  var preview = $('#video-preview');
+  var previewEl = $('.previews');
+
+  var media = [];
+
+  streamer.startVideo(function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      streamer.video = data.videoElement;
+      streamer.video.width = data.videoElement.width;
+      streamer.video.height = data.videoElement.height;
+      preview.append(streamer.video);
+      streamer.video.play();
+    }
+  });
+
+  var saveFrame = function (img, callback) {
+    media.push({
+      frame: img.src,
+      geo: recorder.lat + ',' + recorder.lon,
+      description: '',
+      created: Math.round(new Date().getTime() / 1000)
+    });
+
+    callback();
+  };
+
+  var captureFrame = function (pendingFrames, callback) {
+    pendingFrames --;
+    var currFrame = (pendingFrames - 1) + pendingFrames;
+    var img = document.createElement('img');
+
+    canvas.getContext('2d').drawImage(streamer.video, 0, 0, canvas.width, canvas.height);
+    img.src = canvas.toDataURL('image/jpeg', 0.4);
+
+    if (pendingFrames > 0) {
+      setTimeout(function () {
+        if (recorder.hasGeo) {
+          navigator.geolocation.getCurrentPosition(function (pos) {
+            recorder.lat = pos.coords.latitude;
+            recorder.lon = pos.coords.longitude;
+            saveFrame(img, function () {
+              captureFrame(pendingFrames, callback);
+            });
+          });
+        } else {
+          saveFrame(img, function () {
+            captureFrame(pendingFrames, callback);
+          });
+        }
+      }, interval * 1000); // timeouts are in milliseconds
+    } else {
+      var created = Date.now();
+      asyncStorage.getItem('frameList', function (frames) {
+        if (frames) {
+          frames.unshift(created);
+          asyncStorage.setItem('frameList', frames);
+        } else {
+          asyncStorage.setItem('frameList', [created]);
+        }
+      });
+
+      recorder.videoFrames.id = created;
+      asyncStorage.setItem('frames[' + recorder.videoFrames.id + ']', recorder.videoFrames);
+      callback(true);
+    }
+  };
+
+  var getScreenshot = function (callback) {
+    var pendingFrames = 49;
+
+    if (streamer.video) {
+      canvas.width = streamer.video.width;
+      canvas.height = streamer.video.height;
+
+      captureFrame(pendingFrames, callback);
+    }
+  };
+
+  $('#record').click(function (ev) {
+    ev.preventDefault();
+
+    recorder.video = streamer.video;
+    recorder.getScreenshot(function () {
+      console.log('posting done');
+    }, 100, recorder.interval);
+    alert('recording!');
+  });
+});
+
---
+++ js/recorder.js
@@ -0,0 +1,92 @@
+define(['jquery', 'asyncStorage'],
+  function ($, asyncStorage) {
+  'use strict';
+
+  var Recorder = function () {
+    // This is where we change the time lapse count
+    var interval = 5.0; // In seconds
+    var canvas = document.createElement('canvas');
+    var previews = $('.previews');
+
+    this.video;
+    this.hasGeo = false;
+    this.videoFrames = {
+      media: []
+    };
+    this.lat = 0;
+    this.lon = 0;
+    this.streamer;
+
+    var self = this;
+
+    var saveFrame = function (img, callback) {
+      self.videoFrames.media.push({
+        frame: img.src,
+        geo: self.lat + ',' + self.lon,
+        description: '',
+        created: Math.round(new Date().getTime() / 1000)
+      });
+
+      previews.append(img);
+
+      callback();
+    };
+
+    var captureFrame = function (pendingFrames, callback) {
+      pendingFrames --;
+      var currFrame = (pendingFrames - 1) + pendingFrames;
+      var img = document.createElement('img');
+
+      canvas.getContext('2d').drawImage(self.video, 0, 0, canvas.width, canvas.height);
+      img.src = canvas.toDataURL('image/jpeg', 0.4);
+
+      if (pendingFrames > 0) {
+        setTimeout(function () {
+          if (self.hasGeo) {
+            navigator.geolocation.getCurrentPosition(function (pos) {
+              self.lat = pos.coords.latitude;
+              self.lon = pos.coords.longitude;
+              saveFrame(img, function () {
+                captureFrame(pendingFrames, callback);
+              });
+            });
+          } else {
+            saveFrame(img, function () {
+              captureFrame(pendingFrames, callback);
+            });
+          }
+        }, interval * 1000); // timeouts are in milliseconds
+      } else {
+        var created = Date.now();
+        asyncStorage.getItem('frameList', function (frames) {
+          if (frames) {
+            frames.unshift(created);
+            asyncStorage.setItem('frameList', frames);
+          } else {
+            asyncStorage.setItem('frameList', [created]);
+          }
+        });
+
+        self.videoFrames.id = created;
+        asyncStorage.setItem('frames[' + self.videoFrames.id + ']', self.videoFrames);
+        callback(true);
+      }
+    };
+
+    this.getScreenshot = function (callback) {
+      var pendingFrames = 49;
+
+      if (navigator.geolocation) {
+        this.hasGeo = true;
+      };
+
+      if (this.video) {
+        canvas.width = this.video.width;
+        canvas.height = this.video.height;
+        captureFrame(pendingFrames, callback);
+      }
+    };
+  };
+
+  return Recorder;
+});
